21448a8caa2e0368eac6c3a722634f680e60dd1f
Robert Woodcock <woodcockr@users.noreply.github.com>
2025-05-29 22:35:17 +0000

    Mods to compare full glint dsf run (production settings) against refactored code.

--- tests/test_acolite_l2r.py
+++ tests/test_acolite_l2r.py
@@ -9,22 +9,37 @@ import xarray as xr
 from memory_profiler import profile
 
 import acolite as ac
-
+import json
 
 @pytest.fixture()
 def gem():
     test_dir = os.path.dirname(__file__)
     path = os.path.join(test_dir, "data", "L9_OLI_2025_04_29_23_36_04_088080_L1R.nc")
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Test Gem file {path} does not exist.")
     return str(path)
 
 @pytest.fixture()
 def original_dataset_filename():
     test_dir = os.path.dirname(__file__)
-    path = os.path.join(test_dir, "data", "L9_OLI_2025_04_29_23_36_04_088080_L2R_original.nc")
+    path = os.path.join(test_dir, "data", "L9_OLI_2025_04_29_23_36_04_088080_L2R_main_branch_glint.nc")
+    # path = os.path.join(test_dir, "data", "L9_OLI_2025_04_29_23_36_04_088080_L2R_original.nc")
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Original dataset file {path} does not exist.")
     return str(path)
 
+@pytest.fixture()
+def csiro_settings_ls9():
+    test_dir = os.path.dirname(__file__)
+    path = os.path.join(test_dir, "data", "csiro_ls9_settings.json")
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"CSIRO LS9 settings file {path} does not exist.")
+    with open(path, "r") as f:
+        settings = json.load(f)
+    return settings
+
 @profile
-def test_acolite_l2r_runs(gem, original_dataset_filename):
+def test_acolite_l2r_runs(gem, original_dataset_filename, csiro_settings_ls9):
     """
     Test the Acolite L2R module.
     """
@@ -37,7 +52,9 @@ def test_acolite_l2r_runs(gem, original_dataset_filename):
         profiler = cProfile.Profile()
         profiler.enable()
 
-        result = ac.acolite.acolite_l2r(gem, output=temp_dir)
+        result = ac.acolite.acolite_l2r(gem, output=temp_dir, settings=csiro_settings_ls9)
+        # settings = {"dsf_write_aot_550": True}
+        # result = ac.acolite.acolite_l2r(gem, output=temp_dir, settings=settings) # Use default settings only
 
         profiler.disable()
         stats = pstats.Stats(profiler)
@@ -52,4 +69,13 @@ def test_acolite_l2r_runs(gem, original_dataset_filename):
         result_dataset = xr.open_dataset(result[0])
         original_dataset = xr.open_dataset(original_dataset_filename)
 
+        # Remove attribute that differ between runs
+        result_noatts = result_dataset.drop_attrs(deep=True)
+        original_noatts = original_dataset.drop_attrs(deep=True)
+
+        # Check if the datasets are equal
+        for k in result_noatts.data_vars:
+            print(f"{k}, result: {result_noatts[k].equals(original_noatts[k])}")
+
         assert result_dataset.equals(original_dataset), "Output dataset does not match the original dataset."
+        # assert result_dataset.identical(original_dataset), "Output dataset does not match the original dataset."
