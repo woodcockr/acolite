1bfd40f68a5babbae6806e8cb43ab087eccfecc4
Robert Woodcock <woodcockr@users.noreply.github.com>
2025-06-06 04:03:29 +0000

    Disable memory_profiler

--- tests/test_acolite_l2r.py
+++ tests/test_acolite_l2r.py
@@ -37,7 +37,7 @@ def csiro_settings_ls9():
         settings = json.load(f)
     return settings
 
-@profile
+# @profile
 def test_acolite_l2r_runs(gem, original_dataset_filename, csiro_settings_ls9):
     """
     Test the Acolite L2R module.
@@ -69,12 +69,12 @@ def test_acolite_l2r_runs(gem, original_dataset_filename, csiro_settings_ls9):
         original_dataset = xr.open_dataset(original_dataset_filename)
 
         # Remove attribute that differ between runs
-        result_noatts = result_dataset.drop_attrs(deep=True)
-        original_noatts = original_dataset.drop_attrs(deep=True)
+        # result_noatts = result_dataset.drop_attrs(deep=True)
+        # original_noatts = original_dataset.drop_attrs(deep=True)
 
         # Check if the datasets are equal
-        for k in original_noatts.data_vars:
-            print(f"{k}, result: {result_noatts[k].equals(original_noatts[k])}")
+        # for k in original_noatts.data_vars:
+        #     print(f"{k}, result: {result_noatts[k].equals(original_noatts[k])}")
 
         assert result_dataset.equals(original_dataset), "Output dataset does not equal the original dataset."
 
