16178c91dd30a22cdc793bf1cc79a28eeaee2f54
Robert Woodcock <woodcockr@users.noreply.github.com>
2025-06-14 07:13:17 +0000

    Assume different worker counts will suit different sections of the processing, rename the max workers configuration for each major section of code.

--- acolite/sentinel2/l1_convert.py
+++ acolite/sentinel2/l1_convert.py
@@ -424,7 +424,7 @@ def l1_convert(inputfile, output = None, settings = None,
 
                 ## use target band so we can just do the 60 metres geometry
                 if os.path.exists(target_file):
-                    with ThreadPoolExecutor(max_workers=setu["acolite-mp_max_workers"]) as executor:
+                    with ThreadPoolExecutor(max_workers=setu["acolite-mp_l1_convert_max_workers"]) as executor:
                         results = list(executor.map(
                             warp_from_source_parallel,
                             [
@@ -703,7 +703,7 @@ def l1_convert(inputfile, output = None, settings = None,
             for b in rsr_bands
         ]
 
-        with concurrent.futures.ThreadPoolExecutor() as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=setu["acolite-mp_l1_convert_max_workers"]) as executor:
             results = list(executor.map(lambda args: process_band_s2(*args), band_args))
 
         for res in results:
